9151817ba1443a137e429c88976ae5371e435f8c
RobinJot <rjeremicz@wp.pl>
2024-08-05 20:47:26 +0200

    Zadanie domowe dzień 4

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿var name = "Ewa";
+var age = 20;
+//bool isMan = true;  //k - kobieta; m - mężczyzna
+var sex = "k";  //k - kobieta; m - mężczyzna
+
+if ( name == "Ewa" && age == 30 )
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if ( sex == "k" && age < 30 )
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if ( sex == "m" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("nic!");
+}
